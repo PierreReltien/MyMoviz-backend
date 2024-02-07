b35b72293159ecc607c7eba6f83177945ed54649
RELTIEN <pierre.reltien@gmail.com>
2024-02-07 14:13:31 +0100

    cors solution

--- app.js
+++ app.js
@@ -9,7 +9,14 @@ var indexRouter = require('./routes/index');
 var app = express();
 
 const cors = require('cors');
-app.use(cors());
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 app.use(logger('dev'));
 app.use(express.json());
